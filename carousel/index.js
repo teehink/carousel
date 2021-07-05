583a05ee3cfe3ee9071cc991d6bd0666587a0cfc
tristan <teehink@gmail.com>
2021-07-05 10:30:35 -1000

    Functional code

---
+++ carousel/index.js
@@ -0,0 +1,39 @@
+const slides = document.getElementsByClassName("carousel-item")
+let slidePosition = 0
+const totalSlides = slides.length
+
+const nextSlide = document.getElementById("carousel-btn-next")
+const prevSlide = document.getElementById("carousel-btn-prev")
+
+nextSlide.addEventListener("click", moveToNextSlide )
+prevSlide.addEventListener("click", moveToPrevSlide )
+
+
+function moveToNextSlide() {
+    hideAllSlides()
+    if (slidePosition = totalSlides - 1) {
+        slidePosition === 0
+    } else {
+        slidePosition ++
+    }
+
+    slides[slidePosition].classList.add("carousel-item-visible")
+}
+
+function moveToPrevSlide() {
+    hideAllSlides()
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1
+    } else {
+        slidePosition --
+    }
+
+    slides[slidePosition].classList.add("carousel-item-visible")
+}
+
+function hideAllSlides() {
+    for (let slide of slides) {
+        slide.classList.remove("carousel-item-visible")
+        slide.classList.add("carousel-item-hidden")
+    }
+}
